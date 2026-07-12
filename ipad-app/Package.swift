a4ea4954eae1ca81b0ec2ed44d59f7993b6a3dab
Ashutosh Singh <83775661+Ashutoshsingh20@users.noreply.github.com>
2026-07-12 15:24:49 +0530

    build: separate iPad app from core Swift package

--- ipad-app/Package.swift
+++ ipad-app/Package.swift
@@ -2,19 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "KalpanaDrive",
+    name: "KalpanaDriveCorePackage",
     platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(name: "KalpanaDriveCore", targets: ["KalpanaDriveCore"]),
-        .executable(name: "KalpanaDriveApp", targets: ["KalpanaDriveApp"]),
         .executable(name: "KalpanaDriveChecks", targets: ["KalpanaDriveChecks"])
     ],
     targets: [
         .target(name: "KalpanaDriveCore"),
-        .executableTarget(
-            name: "KalpanaDriveApp",
-            dependencies: ["KalpanaDriveCore"]
-        ),
         .executableTarget(
             name: "KalpanaDriveChecks",
             dependencies: ["KalpanaDriveCore"],
